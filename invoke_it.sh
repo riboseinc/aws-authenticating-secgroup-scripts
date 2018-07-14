202f3998f11283962d1c911d6413159f089df539
Phuong H <phuonghqh@gmail.com>
2018-07-14 18:42:04 +0700

    Fix readonly var

--- invoke_it.sh
+++ invoke_it.sh
@@ -128,30 +128,30 @@ function main() {
         exit 1
     fi
 
-    readonly method="${method}"
-    readonly aws_access_key=$(cut -d':' -f1 <<<"${credentials}")
-    readonly aws_secret_key=$(cut -d':' -f2 <<<"${credentials}")
-    readonly api_url="${url}"
+    local method="${method}"
+    local aws_access_key=$(cut -d':' -f1 <<<"${credentials}")
+    local aws_secret_key=$(cut -d':' -f2 <<<"${credentials}")
+    local api_url="${url}"
     log "aws_access_key=" "${aws_access_key}"
     log "aws_secret_key=" "${aws_secret_key}"
     log "api_url=" "${api_url}"
 
-    readonly timestamp=${timestamp-$(date -u +"%Y%m%dT%H%M%SZ")} #$(date -u +"%Y%m%dT%H%M%SZ") #"20171226T112335Z"
-    readonly today=${today-$(date -u +"%Y%m%d")}  # $(date -u +"%Y%m%d") #20171226
+    local timestamp=${timestamp-$(date -u +"%Y%m%dT%H%M%SZ")} #$(date -u +"%Y%m%dT%H%M%SZ") #"20171226T112335Z"
+    local today=${today-$(date -u +"%Y%m%d")}  # $(date -u +"%Y%m%d") #20171226
     log "timestamp=" "${timestamp}"
     log "today=" "${today}"
 
-    readonly api_host=$(printf ${api_url} | awk -F/ '{print $3}')
-    readonly api_uri=$(printf ${api_url} | grep / | cut -d/ -f4-)
+    local api_host=$(printf ${api_url} | awk -F/ '{print $3}')
+    local api_uri=$(printf ${api_url} | grep / | cut -d/ -f4-)
 
-    readonly aws_region=$(cut -d'.' -f3 <<<"${api_host}")
-    readonly aws_service=$(cut -d'.' -f2 <<<"${api_host}")
+    local aws_region=$(cut -d'.' -f3 <<<"${api_host}")
+    local aws_service=$(cut -d'.' -f2 <<<"${api_host}")
 
-    readonly algorithm="AWS4-HMAC-SHA256"
-    readonly credential_scope="${today}/${aws_region}/${aws_service}/aws4_request"
+    local algorithm="AWS4-HMAC-SHA256"
+    local credential_scope="${today}/${aws_region}/${aws_service}/aws4_request"
 
-    readonly signed_headers="host;x-amz-date"
-    readonly header_x_amz_date="x-amz-date:${timestamp}"
+    local signed_headers="host;x-amz-date"
+    local header_x_amz_date="x-amz-date:${timestamp}"
 
     invoke_it "${method}"
 
